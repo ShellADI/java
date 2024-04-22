34b0dcf3941e2d41dbc9891b9fbc003f47cc2dd4
ShellADI <adityachetry13@gmail.com>
2024-04-23 01:11:23 +0530

    commit

---
+++ matchString.java
@@ -0,0 +1,19 @@
+class A{
+    String s;
+    A(){
+        java.util.Scanner sc=new java.util.Scanner(System.in);
+        System.out.print("Enter a string: ");
+        s=sc.nextLine();
+        sc.close(); 
+    }
+    void charToArray(){
+        char[] ch=s.toCharArray();
+        for(char i:ch) System.out.print(i+" ");
+    }
+}
+public class matchString {
+    public static void main(String[] args) {
+        A ob=new A();
+        ob.charToArray();
+    }
+}
